e78ceb3a0a1545db365a55208f24c6bbca7c2404
Zile10 <johnnoad04@gmail.com>
2023-02-14 12:08:51 +0200

    Deploy

---
+++ app/config/db.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  HOST: "bnrpenhvw7cvshe8bbkn-mysql.services.clever-cloud.com",
+  USER: "u3jlct8whq0htey0",
+  PASSWORD: "ik5sou7L2Zh9yUQty0wa",
+  DB: "bnrpenhvw7cvshe8bbkn"
+};
